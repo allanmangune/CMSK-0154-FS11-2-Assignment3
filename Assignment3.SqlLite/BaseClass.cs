c7491a9b633a1eb0fd44ef3b2b59a800b7e4559a
allan <allanmangune@hotmail.com>
2024-03-17 17:09:43 -0600

    Added a new project to demonstrate the use of a local database.

---
+++ Assignment3.SqlLite/BaseClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment3{
+    /// <summary>
+    /// A base class with a virtual IsValidated method.
+    /// </summary>
+    public class BaseClass
+    {
+        /// <summary>
+        /// Determines if the object is in a validated state.
+        /// </summary>
+        /// <returns>Always returns false in the base implementation.</returns>
+        public virtual bool IsValidated()
+        {
+            return false;
+        }
+    }
+}
+
+
